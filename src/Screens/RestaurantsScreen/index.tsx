08f9426d44795d2883c802b9024f80f3c130b8fa
Ahmed Hamed <ahmedhamed7728@gmail.com>
2021-11-07 23:18:35 +0700

    restaurants screen design

--- src/Screens/RestaurantsScreen/index.tsx
+++ src/Screens/RestaurantsScreen/index.tsx
@@ -1,8 +1,19 @@
 import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ListRenderItem,
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
+import {Restaurant} from '../../api/restaurants/types';
 import {getRestaurantsList} from '../../redux/resturant/action';
 import {RootState} from '../../redux/rootReducer';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {styles} from './style';
 
 export const RestaurantsScreen = () => {
   const dispatch = useDispatch();
@@ -14,10 +25,42 @@ export const RestaurantsScreen = () => {
     dispatch(getRestaurantsList());
   }, [dispatch]);
 
-  console.log(restaurants, '===restaurants');
+  const renderItem: ListRenderItem<Restaurant> = ({item}) => {
+    return (
+      <TouchableOpacity style={styles.touchableOpacityContainer}>
+        <View style={styles.containerHeaderCard}>
+          <Image
+            source={{uri: item.image, width: 120, height: 120}}
+            borderRadius={8}
+          />
+          <MaterialIcons name="favorite" size={22} color={'red'} />
+        </View>
+
+        <View style={styles.containerFooterCard}>
+          <Text style={styles.textCardStyle}>{item.restauranttype}</Text>
+          <Text style={styles.textCardStyle}>{item.businessname}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  if (loading) {
+    return <ActivityIndicator style={styles.handlerContainer} />;
+  }
+
+  if (error) {
+    return (
+      <View style={styles.handlerContainer}>
+        <Text>Error fetching data</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>RestaurantsScreen</Text>
-    </View>
+    <FlatList
+      contentContainerStyle={styles.container}
+      data={restaurants}
+      keyExtractor={item => item.id}
+      renderItem={renderItem}
+    />
   );
 };
--- src/Screens/RestaurantsScreen/style.ts
+++ src/Screens/RestaurantsScreen/style.ts
@@ -1,5 +1,36 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    paddingHorizontal: 12,
+  },
+  touchableOpacityContainer: {
+    paddingVertical: 12,
+    paddingHorizontal: 8,
+    marginBottom: 12,
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 4,
+  },
+  containerHeaderCard: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  containerFooterCard: {
+    flex: 1,
+    paddingVertical: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textCardStyle: {
+    fontSize: 14,
+    lineHeight: 17,
+    color: '#000000',
+    fontWeight: 'bold',
+  },
+  handlerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
